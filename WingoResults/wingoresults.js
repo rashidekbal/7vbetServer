4ec09af02bcc1b9c339d631a5eccae605c1214b0
rashid ekbal <rtechdevlopment123@gmail.com>
2024-11-16 01:49:19 +0530

    fixed period problem in 1min or any result

--- WingoResults/wingoresults.js
+++ WingoResults/wingoresults.js
@@ -18,10 +18,15 @@ export { wingo };
 function WingoOneMinResult() {
   let result;
   let date = new Date();
+  let year = date.getFullYear();
+  let month = date.getMonth() + 1;
+  let day = date.getDate();
+  let hour = date.getHours();
+  let min = date.getMinutes() + 1;
   // generate new random int between 0 and 9
-  let period = `${date.getFullYear()}${
-    date.getMonth() + 1
-  }${date.getDate()}${date.getHours()}${date.getMinutes() + 1}`;
+  let period = `${year}${month}${day}${hour == 0 ? `00` : hour}${
+    min == 0 ? `60` : min
+  }`;
   let number = Math.floor(Math.random() * 10);
   let size;
   let color;
@@ -45,6 +50,48 @@ function WingoOneMinResult() {
   result = { period, number: `${String(number)}`, size, color };
 
   let insert = `insert into wingo1min values ('${period}','${number}','${size}','${color}')`;
+  connection.query(insert, (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+    }
+  });
+}
+function WingoFiveMinResult() {
+  let result;
+  let date = new Date();
+  let year = date.getFullYear();
+  let month = date.getMonth() + 1;
+  let day = date.getDate();
+  let hour = date.getHours();
+  let min = date.getMinutes() + 1;
+  // generate new random int between 0 and 9
+  let period = `${year}${month}${day}${hour == 0 ? `00` : hour}${
+    min == 0 ? `60` : min
+  }`;
+  let number = Math.floor(Math.random() * 10);
+  let size;
+  let color;
+
+  if (number < 5) {
+    size = "small";
+  } else {
+    size = "big";
+  }
+  if (number % 2 == 0) {
+    color = "red";
+  } else {
+    color = "green";
+  }
+  if (number == 0) {
+    color = "redViolet";
+  } else if (number == 5) {
+    color = "greenViolet";
+  }
+
+  result = { period, number: `${String(number)}`, size, color };
+
+  let insert = `insert into wingo5min values ('${period}','${number}','${size}','${color}')`;
   connection.query(insert, (err, result) => {
     if (err) {
       console.log(err);
