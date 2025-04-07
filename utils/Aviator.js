0097e59890a92882541e33ff8af0445b3671581d
rashid ekbal <rtechdevlopment123@gmail.com>
2025-04-07 16:04:26 +0530

    add feature for delay of new game

--- utils/Aviator.js
+++ utils/Aviator.js
@@ -1,3 +1,21 @@
+let period;
+let initial_height;
+let final_height;
+function generator_period() {
+  let date = new Date();
+  let year = date.getFullYear();
+  let month = date.getMonth() + 1;
+  let day = date.getDate();
+  let hour = date.getHours();
+  let min = date.getMinutes() + 1;
+  let sec = date.getSeconds();
+  // generate new random int between 0 and 9
+  let period = `${year}${month}${day}${hour == 0 ? `00` : hour}${
+    min == 0 ? `60` : min < 10 ? "0" + min : min
+  }${sec}`;
+  return period;
+}
+
 function get_up_to_to_digits_decimal(x) {
   return parseFloat(x.toFixed(2));
 }
@@ -7,13 +25,15 @@ function generateFlight(x) {
   return result;
 }
 function fly_the_bird(result, socket) {
-  console.log(result);
-  let initial_height = 1.0;
-  let final_height = initial_height;
+  initial_height = 1.0;
+  final_height = initial_height;
 
   let flight_controller = setInterval(() => {
     if (get_up_to_to_digits_decimal(final_height) == result) {
       clearInterval(flight_controller);
+      socket.emit("Game_over", {
+        msg: true,
+      });
       start_new_game(socket);
     } else {
       final_height = get_up_to_to_digits_decimal(final_height + 0.01);
@@ -28,18 +48,24 @@ function fly_the_bird(result, socket) {
 }
 function start_new_game(socket) {
   let result = generateFlight(4);
+  console.log("upcoming result", result);
+  period = generator_period();
+
   let currentSec = 0;
-  let Running_interval = setInterval(() => {
-    if (currentSec == 10) {
-      clearInterval(Running_interval);
-      fly_the_bird(result, socket);
-    }
-    if (currentSec < 10) {
-      currentSec++;
-      socket.emit("Aviator_halt", {
-        remaining_time: Math.abs(currentSec - 10),
-      });
-    }
+  setTimeout(() => {
+    let Running_interval = setInterval(() => {
+      if (currentSec == 10) {
+        clearInterval(Running_interval);
+        fly_the_bird(result, socket);
+      }
+      if (currentSec < 10) {
+        currentSec++;
+        socket.emit("Aviator_halt", {
+          remaining_time: Math.abs(currentSec - 10),
+        });
+        socket.emit("period", { period: period });
+      }
+    }, 1000);
   }, 1000);
 }
 export default start_new_game;
